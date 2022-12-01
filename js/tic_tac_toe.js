9b48be505658c4d1fc3049985a44dda733197e1b
Jonny <jrheberer@gmail.com>
2022-12-01 15:37:48 -0500

    Added draw condition--app will inform you when the game has ended in a draw.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,23 +1,28 @@
 'use strict';
 
-let playerTurn = ['X'];
-let numberOfMoves = 0;
+
+let gameParameters = {playerTurn: 'X', numberOfMoves:0}
 const gameBoard = [[null,null,null],[null,null,null],[null,null,null]];
 
 
-        
-// function checkWinCondition (gameboard) {
-//     // 3 across
-//     for (let row in gameBoard) {
-//         for (let square in row) {
-//             if square === 
-//         }
-//     }
+// this function uses global variable. should probably change it
+function checkWinCondition (parameters) {
+    // 3 across
+    // for (let row in gameBoard) {
+    //     for (let square in row) {
+    //         if square === 
+    //     }
+    // }
 
-//     // 3 down
+    // 3 down
 
-//     // 2 diagonals
-// }
+    // 2 diagonals
+
+    // draw
+    if (parameters.numberOfMoves === 9) {
+        window.alert('Game has ended in a draw! Please play again.')
+    }
+}
 
 window.onload = () => {
     const restartButton = document.getElementById('restart_button');
@@ -51,23 +56,25 @@ function locationValidator(element) {
 // }
 
 
-function addMarker (element, turnIndicator) {
+function addMarker (element, gameParameters) {
     if (locationValidator(element)) {
-        if (turnIndicator[0] === 'X') {
+        if (gameParameters.playerTurn === 'X') {
             addX(element);
-            turnIndicator[0] = 'O';
+            gameParameters.playerTurn = 'O';
             document.getElementById('player_indicator').innerHTML = "Player's Turn: O";
-            numberOfMoves ++;
+            gameParameters.numberOfMoves ++;
         }
         else {
             addO(element);
-            turnIndicator[0] = 'X';
+            gameParameters.playerTurn = 'X';
             document.getElementById('player_indicator').innerHTML = "Player's Turn: X";
-            numberOfMoves ++;
+            gameParameters.numberOfMoves ++;
         }
+    // fix this so that the alert comes after the DOM is updated
+    checkWinCondition(gameParameters);
     }
+    
 }
-
 
 function restartGame () {
     window.alert('Restarting game!')
